4f21d57f41b13cffc1705a2e755e23e62446154e
xasteras <xasteras@gmail.com>
2025-04-23 12:15:56 +0300

    google login + comments routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,3 +7,8 @@ router.post('/login', authController.login);
 router.get('/google/callback', authController.googleLogin);
 
 module.exports = router
+
+
+// Αυτό αν το κάνουμε paste σε ένα browser μπαίνουμε στα στοιχεία του user με το payload, με πηγαίνει στο localhost:3000/api/auth/google/callback?code.....
+
+// https://accounts.google.com/o/oauth2/auth?client_id=49291455257-p295u853vt861qredqvkvg24m29req43.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
